77d6b6b40781c8f79610f89ec6eaf5db9a3b0ed8
Ishika Goyal <goyalishika2004@gmail.com>
2024-07-11 05:03:38 +0530

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      
+  },
+  plugins: [],
+  experimental:{
+    applyComplexClasses:true,
+  }
+
+}
+
+
+}
+
